9e936c0cfcee447a0de66813a8847a6fe0e8e473
peng00bo00 <pengbo_tongji@126.com>
2023-10-15 14:51:01 +0400

    Update UniquePtr.hpp

--- SimpleSTL/UniquePtr.hpp
+++ SimpleSTL/UniquePtr.hpp
@@ -5,4 +5,30 @@
 namespace SimpleSTL
 {
 
+template <typename T>
+struct DefaultDeleter {
+    void operator() (T *p) const {
+        delete p;
+    }
+};
+
+template <typename T>
+struct DefaultDeleter<T[]> {
+    void operator() (T *p) const {
+        delete[] p;
+    }
+};
+
+template <>
+struct DefaultDeleter<FILE> {
+    void operator()(FILE *p) const {
+        fclose(p);
+    }
+};
+
+template <typename T, typename Deleter = DefaultDeleter<T>>
+struct UniquePtr {
+
+};
+
 }
